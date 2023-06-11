369522678641161b9c279fd80d8d036202f993a1
abdurahman-hassan <abdelrahman15376@fagr.bu.edu.eg>
2023-06-11 03:47:07 +0300

    Add admin file

---
+++ blog/admin.py
@@ -0,0 +1,14 @@
+from django.contrib import admin
+from blog.models import Tag, Post, Comment
+
+# Register your models here.
+
+
+class PostAdmin(admin.ModelAdmin):
+    prepopulated_fields = {"slug": ("title",)}
+    list_display = ('slug', 'published_at')
+
+
+admin.site.register(Tag)
+admin.site.register(Post, PostAdmin)
+admin.site.register(Comment)
