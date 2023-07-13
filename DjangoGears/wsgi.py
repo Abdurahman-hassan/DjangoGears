2b8b28e71d1ac9ddc3cb86989d7212c6fc5806f5
abdurahman-hassan <abdelrahman15376@fagr.bu.edu.eg>
2023-07-13 23:53:34 +0300

    update the configuration

--- DjangoGears/wsgi.py
+++ DjangoGears/wsgi.py
@@ -1,16 +1,21 @@
+
 """
-WSGI config for DjangoGears project.
+WSGI config for blango project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
 For more information on this file, see
-https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
+https://docs.djangoproject.com/en/3.2/howto/deployment/wsgi/
 """
 
 import os
 
-from django.core.wsgi import get_wsgi_application
+# from django.core.wsgi import get_wsgi_application
+
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'DjangoGears.settings')
+os.environ.setdefault("DJANGO_CONFIGURATION", "Prod")
+
+from configurations.wsgi import get_wsgi_application
 
 application = get_wsgi_application()
