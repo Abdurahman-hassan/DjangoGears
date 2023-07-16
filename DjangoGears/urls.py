6c78670bfbc876856063fa09c679ede0fb9b47c7
abdurahman-hassan <abdelrahman15376@fagr.bu.edu.eg>
2023-07-17 01:29:26 +0300

    Add debug_mode url

--- DjangoGears/urls.py
+++ DjangoGears/urls.py
@@ -16,12 +16,20 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 import blog.views
-
+import debug_toolbar
+from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("", blog.views.index),
     path("post/<slug>/", blog.views.post_detail, name="blog-post-detail"),
-    path("__debug__/", include("debug_toolbar.urls")),
+    # path("__debug__/", include("debug_toolbar.urls")),
+    path("ip/", blog.views.get_ip)
+
 
 ]
+
+if settings.DEBUG:
+    urlpatterns += [
+        path("__debug__/", include(debug_toolbar.urls)),
+    ]
